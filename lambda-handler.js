00ff0edd876877d5eedc7e94cfb358406a48744c
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2025-07-02 17:21:03 +0000

    testing if this change fixes white page issue.

--- lambda-handler.js
+++ lambda-handler.js
@@ -1,21 +1,7 @@
-import serverlessExpress from "aws-serverless-express";
+import serverlessExpress from "@vendia/serverless-express";
 import app from "./server-lambda.js";
 
 console.log("Lambda handler initializing...");
-const server = serverlessExpress.createServer(app);
 console.log("Express server created for Lambda");
 
-export const handler = (event, context) => {
-  console.log("Lambda handler invoked:", {
-    httpMethod: event.httpMethod,
-    path: event.path,
-    headers: event.headers,
-  });
-
-  try {
-    return serverlessExpress.proxy(server, event, context, "PROMISE").promise;
-  } catch (error) {
-    console.error("Lambda handler error:", error);
-    throw error;
-  }
-};
+export const handler = serverlessExpress({ app });
