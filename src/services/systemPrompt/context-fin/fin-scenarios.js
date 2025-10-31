ae93f7597d84ff067100b51b40dc4f23b1a172c4
Lisa Fast <lisa@vation.ca>
2025-10-31 08:53:22 -0700

    fix: tweak fin scenario

--- src/services/systemPrompt/context-fin/fin-scenarios.js
+++ src/services/systemPrompt/context-fin/fin-scenarios.js
@@ -1,9 +1,9 @@
 export const FIN_SCENARIOS = `
 ### Budget announcement 
-- A new Government of Canada budget announcement will be published on Nov 4 at 4pm EST 
-- A link to the budget home page and to the PDF versions will be listed on the main Budget page - which until that time will only show all previous budgets. (Updated Nov 4 2025 4pm EST) https://www.budget.canada.ca/home-accueil-en.html https://budget.canada.ca/home-accueil-fr.html
-
-- Questions about budget details should be answered in the context that this is an announcement that must be put into effect by the government, do not speculate or be definitive about what will or won't be achieved. 
+- A new Government of Canada 2025 budget announcement will be published on Nov 4 at 4pm EST 
+- A link to the 2025 budget home page and to the PDF versions will be listed on the main Budget page at that time. Until the announcement is published this main page will only show all previous budgets. (Updated Nov 4 2025 4pm EST) https://www.budget.canada.ca/home-accueil-en.html https://budget.canada.ca/home-accueil-fr.html
+- Do not speculate about the budget announcement - only answer questions about 2025 after the announcement pages are published. 
+- After the announcement is published, questions about budget details should be answered in the context that this is an announcement that must be put into effect by the government, do not speculate or be definitive about what will or won't be achieved. 
 
 - The following pages will go live after the official announcement at 16:00 EST - provide these URLS as citations to questions about the new 2025 budget only after that time: 
 
