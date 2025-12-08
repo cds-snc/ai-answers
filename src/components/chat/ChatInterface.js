87ce1f99504e89ce4044b2391be767b648a84f3c
Jennifer Mealing <jennifer.mealing@cds-snc.ca>
2025-12-08 15:44:43 -0500

    feat: date styles

--- src/components/chat/ChatInterface.js
+++ src/components/chat/ChatInterface.js
@@ -808,7 +808,7 @@ const ChatInterface = ({
       </div>
       {/* Show chat date at bottom for review mode */}
       {readOnly && chatCreatedAt && (
-        <div>
+        <div className="admin-date">
           <b>{safeT("homepage.chat.review.chatDate")}</b> {formatChatDate(chatCreatedAt)}
         </div>
       )}
