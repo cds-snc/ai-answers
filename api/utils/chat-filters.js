6f77b517fed1049e1e47c5a9ba0f2600fd70b2a1
Ryan Hyma <Ryan.Hyma@gmail.com>
2026-01-07 08:28:26 -0500

    feat: Add chat filtering and evaluation categorization utilities, including MongoDB aggregation expressions.

--- api/utils/chat-filters.js
+++ api/utils/chat-filters.js
@@ -109,35 +109,43 @@ export function getPartnerEvalAggregationExpression() {
           in: {
             $let: {
               vars: {
-                hasAnyScore: { $or: [
-                  { $in: ['$$ef.sentence1Score', [0, 80, 100]] },
-                  { $in: ['$$ef.sentence2Score', [0, 80, 100]] },
-                  { $in: ['$$ef.sentence3Score', [0, 80, 100]] },
-                  { $in: ['$$ef.sentence4Score', [0, 80, 100]] },
-                  { $in: ['$$ef.citationScore', [0, 20, 25]] },
-                  { $ne: ['$$ef.totalScore', null] }
-                ] },
+                hasAnyScore: {
+                  $or: [
+                    { $in: ['$$ef.sentence1Score', [0, 80, 100]] },
+                    { $in: ['$$ef.sentence2Score', [0, 80, 100]] },
+                    { $in: ['$$ef.sentence3Score', [0, 80, 100]] },
+                    { $in: ['$$ef.sentence4Score', [0, 80, 100]] },
+                    { $in: ['$$ef.citationScore', [0, 20, 25]] },
+                    { $ne: ['$$ef.totalScore', null] }
+                  ]
+                },
                 hasPerfectTotalScore: { $eq: ['$$ef.totalScore', 100] },
                 hasCitationError: { $eq: ['$$ef.citationScore', 0] },
-                hasHarmful: { $or: [
-                  { $eq: ['$$ef.sentence1Harmful', true] },
-                  { $eq: ['$$ef.sentence2Harmful', true] },
-                  { $eq: ['$$ef.sentence3Harmful', true] },
-                  { $eq: ['$$ef.sentence4Harmful', true] }
-                ] },
-                hasError: { $or: [
-                  { $eq: ['$$ef.sentence1Score', 0] },
-                  { $eq: ['$$ef.sentence2Score', 0] },
-                  { $eq: ['$$ef.sentence3Score', 0] },
-                  { $eq: ['$$ef.sentence4Score', 0] }
-                ] },
-                hasNeedsImprovement: { $or: [
-                  { $eq: ['$$ef.sentence1Score', 80] },
-                  { $eq: ['$$ef.sentence2Score', 80] },
-                  { $eq: ['$$ef.sentence3Score', 80] },
-                  { $eq: ['$$ef.sentence4Score', 80] },
-                  { $eq: ['$$ef.citationScore', 20] }
-                ] }
+                hasHarmful: {
+                  $or: [
+                    { $eq: ['$$ef.sentence1Harmful', true] },
+                    { $eq: ['$$ef.sentence2Harmful', true] },
+                    { $eq: ['$$ef.sentence3Harmful', true] },
+                    { $eq: ['$$ef.sentence4Harmful', true] }
+                  ]
+                },
+                hasError: {
+                  $or: [
+                    { $eq: ['$$ef.sentence1Score', 0] },
+                    { $eq: ['$$ef.sentence2Score', 0] },
+                    { $eq: ['$$ef.sentence3Score', 0] },
+                    { $eq: ['$$ef.sentence4Score', 0] }
+                  ]
+                },
+                hasNeedsImprovement: {
+                  $or: [
+                    { $eq: ['$$ef.sentence1Score', 80] },
+                    { $eq: ['$$ef.sentence2Score', 80] },
+                    { $eq: ['$$ef.sentence3Score', 80] },
+                    { $eq: ['$$ef.sentence4Score', 80] },
+                    { $eq: ['$$ef.citationScore', 20] }
+                  ]
+                }
               },
               in: {
                 $cond: {
@@ -175,44 +183,54 @@ export function getAiEvalAggregationExpression() {
       },
       in: {
         $cond: {
-          if: { $or: [
-            { $eq: ['$$autoEval', null] },
-            { $eq: ['$$ef', null] }
-          ] },
+          if: {
+            $or: [
+              { $eq: ['$$autoEval', null] },
+              { $eq: ['$$ef', null] }
+            ]
+          },
           // DocumentDB does not support $$REMOVE; return null instead
           then: null,
           else: {
             $let: {
               vars: {
-                hasAnyScore: { $or: [
-                  { $in: ['$$ef.sentence1Score', [0, 80, 100]] },
-                  { $in: ['$$ef.sentence2Score', [0, 80, 100]] },
-                  { $in: ['$$ef.sentence3Score', [0, 80, 100]] },
-                  { $in: ['$$ef.sentence4Score', [0, 80, 100]] },
-                  { $in: ['$$ef.citationScore', [0, 20, 25]] },
-                  { $ne: ['$$ef.totalScore', null] }
-                ] },
+                hasAnyScore: {
+                  $or: [
+                    { $in: ['$$ef.sentence1Score', [0, 80, 100]] },
+                    { $in: ['$$ef.sentence2Score', [0, 80, 100]] },
+                    { $in: ['$$ef.sentence3Score', [0, 80, 100]] },
+                    { $in: ['$$ef.sentence4Score', [0, 80, 100]] },
+                    { $in: ['$$ef.citationScore', [0, 20, 25]] },
+                    { $ne: ['$$ef.totalScore', null] }
+                  ]
+                },
                 hasPerfectTotalScore: { $eq: ['$$ef.totalScore', 100] },
                 hasCitationError: { $eq: ['$$ef.citationScore', 0] },
-                hasHarmful: { $or: [
-                  { $eq: ['$$ef.sentence1Harmful', true] },
-                  { $eq: ['$$ef.sentence2Harmful', true] },
-                  { $eq: ['$$ef.sentence3Harmful', true] },
-                  { $eq: ['$$ef.sentence4Harmful', true] }
-                ] },
-                hasError: { $or: [
-                  { $eq: ['$$ef.sentence1Score', 0] },
-                  { $eq: ['$$ef.sentence2Score', 0] },
-                  { $eq: ['$$ef.sentence3Score', 0] },
-                  { $eq: ['$$ef.sentence4Score', 0] }
-                ] },
-                hasNeedsImprovement: { $or: [
-                  { $eq: ['$$ef.sentence1Score', 80] },
-                  { $eq: ['$$ef.sentence2Score', 80] },
-                  { $eq: ['$$ef.sentence3Score', 80] },
-                  { $eq: ['$$ef.sentence4Score', 80] },
-                  { $eq: ['$$ef.citationScore', 20] }
-                ] }
+                hasHarmful: {
+                  $or: [
+                    { $eq: ['$$ef.sentence1Harmful', true] },
+                    { $eq: ['$$ef.sentence2Harmful', true] },
+                    { $eq: ['$$ef.sentence3Harmful', true] },
+                    { $eq: ['$$ef.sentence4Harmful', true] }
+                  ]
+                },
+                hasError: {
+                  $or: [
+                    { $eq: ['$$ef.sentence1Score', 0] },
+                    { $eq: ['$$ef.sentence2Score', 0] },
+                    { $eq: ['$$ef.sentence3Score', 0] },
+                    { $eq: ['$$ef.sentence4Score', 0] }
+                  ]
+                },
+                hasNeedsImprovement: {
+                  $or: [
+                    { $eq: ['$$ef.sentence1Score', 80] },
+                    { $eq: ['$$ef.sentence2Score', 80] },
+                    { $eq: ['$$ef.sentence3Score', 80] },
+                    { $eq: ['$$ef.sentence4Score', 80] },
+                    { $eq: ['$$ef.citationScore', 20] }
+                  ]
+                }
               },
               in: {
                 $cond: {
@@ -262,18 +280,18 @@ export function getChatFilterConditions(filters, options = {}) {
   // referringUrl
   if (filters.referringUrl) {
     const escaped = escapeRegex(filters.referringUrl);
-    conditions.push({ [withPath('referringUrl')]: { $regex: escaped, $options: 'i' } });
+    conditions.push({ [withPath('context.referringUrl')]: { $regex: escaped, $options: 'i' } });
   }
 
   // urlEn and urlFr - combine both values using OR, then rely on $and at the top level
   const urlConditions = [];
   if (filters.urlEn) {
     const escaped = escapeRegex(filters.urlEn);
-    urlConditions.push({ [withPath('referringUrl')]: { $regex: escaped, $options: 'i' } });
+    urlConditions.push({ [withPath('context.referringUrl')]: { $regex: escaped, $options: 'i' } });
   }
   if (filters.urlFr) {
     const escaped = escapeRegex(filters.urlFr);
-    urlConditions.push({ [withPath('referringUrl')]: { $regex: escaped, $options: 'i' } });
+    urlConditions.push({ [withPath('context.referringUrl')]: { $regex: escaped, $options: 'i' } });
   }
   if (urlConditions.length === 1) {
     conditions.push(urlConditions[0]);
