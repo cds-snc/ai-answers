b621950d03c3b35dfe8d224619f72a2c1513e2cf
Jennifer Mealing <jennifer.mealing@cds-snc.ca>
2026-01-20 14:44:11 -0500

    feat: placeholder

---
+++ src/components/src/constants/UserFeedbackOptions.js
@@ -0,0 +1,21 @@
+// /**
+//  * User feedback options and score mappings
+//  * This is the single source of truth for public feedback scores and their corresponding keys
+//  */
+
+// export const FEEDBACK_OPTIONS = {
+//   YES: [
+//     { id: 'noCall', score: 1 },
+//     { id: 'noVisit', score: 2 },
+//     { id: 'savedTime', score: 3 },
+//     { id: 'other', score: 4 },
+//   ],
+//   NO: [
+//     { id: 'notWanted', score: 5 },
+//     { id: 'other', score: 6 },
+//     { id: 'notDetailed', score: 7 },
+//     { id: 'confusing', score: 8 },
+//     { id: 'irrelevant', score: 9 },
+//     { id: 'brokenLink', score: 10 },
+//   ]
+// };
