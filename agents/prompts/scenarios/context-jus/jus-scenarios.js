215c09f5946f625dfdb8505fb47977e61b782010
Lisa Fast <lisa@vation.ca>
2026-01-20 20:44:39 -0500

    fix: add content to JUS

--- agents/prompts/scenarios/context-jus/jus-scenarios.js
+++ agents/prompts/scenarios/context-jus/jus-scenarios.js
@@ -1,1 +1,16 @@
-export const JUS_SCENARIOS = ``;
+export const JUS_SCENARIOS = `
+
+### ⚠️ downloadWebPage TOOL-REQUIRED for Q on details of acts, regulations, statutes 
+Examples:
+- Canadian Charter of Rights and Freedoms https://lois-laws.justice.gc.ca/eng/Const/page-12.html https://lois-laws.justice.gc.ca/fra/const/page-12.html https://lois-laws.justice.gc.ca/fra/const/page-12.html
+- Immigration and Refugee Protection Act (S.C. 2001, c. 27) https://lois-laws.justice.gc.ca/eng/acts/I-2.5/index.html https://lois-laws.justice.gc.ca/fra/lois/i-2.5/index.html
+- Income Tax Act https://lois-laws.justice.gc.ca/eng/acts/I-3.3/index.html https://lois-laws.justice.gc.ca/fra/lois/i-3.3/index.html https://lois-laws.justice.gc.ca/PDF/I-3.3.pdf
+- Food and drug regulations https://lois-laws.justice.gc.ca/eng/regulations/C.R.C.,_c._870/index.html https://lois-laws.justice.gc.ca/fra/reglements/C.R.C.%2C_ch._870/index.html https://lois-laws.justice.gc.ca/PDF/C.R.C.,_c._870.pdf
+
+* Notice FR urls for acts & regulations follow pattern, PDF's contain English & French text
+
+### Updates:
+- Criminal Conviction Review (upd. UN 2025) https://www.justice.gc.ca/eng/cj-jp/ccr-rc/index.html https://www.justice.gc.ca/fra/jp-cj/rc-ccr/index.html
+- Fraudsters pretending to be from Justice https://www.justice.gc.ca/eng/contact/fraud.html https://www.justice.gc.ca/fra/contact/fraud.html https://www.justice.gc.ca/fra/contact/fraud.html
+
+`;
