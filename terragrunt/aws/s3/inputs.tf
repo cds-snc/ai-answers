2a700ddbacb92396760eb03356103fb6b8cd4b6e
Ryan Hyma <Ryan.Hyma@gmail.com>
2026-01-26 10:03:01 -0500

    feat: Add VPC ID and private subnet ID input variables for S3 module.

--- terragrunt/aws/s3/inputs.tf
+++ terragrunt/aws/s3/inputs.tf
@@ -1,17 +1,4 @@
-variable "product_name" {
-  description = "The name of the product"
-  type        = string
-}
 
-variable "env" {
-  description = "The environment name"
-  type        = string
-}
-
-variable "billing_code" {
-  description = "The billing code to tag our resources with"
-  type        = string
-}
 
 variable "vpc_id" {
   description = "The VPC ID"
