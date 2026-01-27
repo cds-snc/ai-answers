1f59818c609fdefa32c8870a4f1e60d8cdd81522
Jennifer Mealing <jennifer.mealing@cds-snc.ca>
2026-01-27 11:36:31 -0500

    feat: test inside span

--- src/components/chat/ChatAppContainer.js
+++ src/components/chat/ChatAppContainer.js
@@ -663,7 +663,6 @@ const ChatAppContainer = ({ lang = 'en', chatId, readOnly = false, initialMessag
                 >
                 <span className="citation-url-text font-size-text-xsm-nr">
                   {displayUrl}
-                </span>
                 <span className="sr-only"> ({safeT('homepage.chat.input.opensInNewTab')})</span>
                   <svg 
                       width="12" 
@@ -681,6 +680,7 @@ const ChatAppContainer = ({ lang = 'en', chatId, readOnly = false, initialMessag
                         fill="none"
                       />
                     </svg>
+                 </span>
                 </a>
               </li>
               </ul>
