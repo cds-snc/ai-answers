70a1e5e92a7064795ce8079d0b0e520b85599a47
Lisa Fast <lisa@vation.ca>
2026-02-12 09:42:33 -0500

    fix: cra updates, cds vs ceo

--- agents/prompts/scenarios/context-cds-snc/cds-snc-scenarios.js
+++ agents/prompts/scenarios/context-cds-snc/cds-snc-scenarios.js
@@ -1,6 +1,6 @@
 export const CDS_SNC_SCENARIOS = `
 - Q on GC Forms https://articles.alpha.canada.ca/forms-formulaires/ https://articles.alpha.canada.ca/forms-formulaires/fr/
-- Q on future digital credentials services:  https://www.canada.ca/en/government/system/digital-government/digital-government-innovations/digital-credentials.html https://www.canada.ca/fr/gouvernement/systeme/gouvernement-numerique/innovations-gouvernementales-numeriques/justificatifs-numeriques.html
-
-
+- Q on future digital credentials, sign in services:  https://www.canada.ca/en/government/system/digital-government/digital-government-innovations/digital-credentials.html https://www.canada.ca/fr/gouvernement/systeme/gouvernement-numerique/innovations-gouvernementales-numeriques/justificatifs-numeriques.html
+_ Q on GCKey https://www.canada.ca/en/government/sign-in-online-account/gckey.html https://www.canada.ca/fr/gouvernement/ouvrir-session-dossier-compte-en-ligne.html
+* List of Interac Sign-In partners: Affinity, ATB Financial, BMO, Caisse Alliance, CIBC, Coast Capital Savings, connectFirst, Conexus , Desjardins Group (Caisses Populaires), Libro, Meridian, National Bank of Canada, RBC Royal Bank, Scotiabank, Servus, Simplii Financial, Steinbach, Tangerine, TD Bank Group, UNI, Vancity, Wealthsimple. List may be out of date as partners added/removed. If user asks for list, explain when click Interac Sign-in Partners to register for specific account, will see list. No list published other than in specific accounts.
 `;
