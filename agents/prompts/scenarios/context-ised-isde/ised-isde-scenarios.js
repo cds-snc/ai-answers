bb6b667189541c901a1332156dabf180b2ef684e
Lisa Fast <lisa@vation.ca>
2026-02-19 14:59:45 -0500

    fix: typo

--- agents/prompts/scenarios/context-ised-isde/ised-isde-scenarios.js
+++ agents/prompts/scenarios/context-ised-isde/ised-isde-scenarios.js
@@ -4,5 +4,5 @@ export const ISED_ISDE_SCENARIOS = `
 - Business & industry theme page: https://www.canada.ca/en/services/business.html https://www.canada.ca/fr/services/entreprises.html
 - To confirm existence of a corporation, direct to Search on: https://ised-isde.canada.ca/cc/lgcy/fdrlCrpSrch.html?locale=en_CA https://ised-isde.canada.ca/cc/lgcy/fdrlCrpSrch.html
 - To find Ministers, Parliamentary secretaries & sec of state, inc Minister of Artificial Intelligence and Digital Innovation - ⚠️DOWNLOAD https://ised-isde.canada.ca/site/ised/en https://ised-isde.canada.ca/site/isde/fr
-- Business benefits finder -answer questions to get tailored list of programs & services across Canada https://innovation.ised-isde.canada.ca/s/?language=en_CA https://innovation.ised-isde.canada.ca/s/?language=fr_CA
+- Business benefits finder -answer questions to get tailored list of programs & services, incl provincial support https://innovation.ised-isde.canada.ca/s/?language=en_CA https://innovation.ised-isde.canada.ca/s/?language=fr_CA
 `;
