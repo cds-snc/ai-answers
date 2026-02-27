2ec5901f3baf2bf8f0a588115f01d0e30c36b4a7
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2026-02-27 14:23:36 -0800

    Adding TXT records

--- terragrunt/aws/load_balancer/route53.tf
+++ terragrunt/aws/load_balancer/route53.tf
@@ -23,3 +23,20 @@ resource "aws_route53_record" "ai_answers_alt" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "ai-answers-letsencrypt-bootstrap" {
+  zone_id = var.hosted_zone_id
+  name    = "_acme-challenge.ai-answers.alpha.canada.ca."
+  type    = "TXT"
+  ttl     = 60
+  records = ["ucK5ahruXj2nYk07dr_UFK63d1jnkyr6lXXDPUMrSzk"]
+}
+
+resource "aws_route53_record" "ai-answers-akamai-domain-ownership" {
+  zone_id = var.hosted_zone_id
+  name    = "_akamai-host-challenge.ai-answers.alpha.canada.ca"
+  type    = "TXT"
+  ttl     = 60
+  records = ["1WxKKGqmCDZaioNzps71yXa88kSG9alusI7yJsm1r-SV8GaIIlmGeAA"]
+
+}
