f72aaa08761615a00406265e33a76f74dc73bf34
Lisa Fast <lisa@vation.ca>
2026-03-06 21:48:25 -0500

    fix: final clean-up dates scenarios

--- agents/prompts/scenarios/context-ised-isde/ised-isde-scenarios.js
+++ agents/prompts/scenarios/context-ised-isde/ised-isde-scenarios.js
@@ -1,13 +1,17 @@
 export const ISED_ISDE_SCENARIOS = `
-- Contact page https://ised-isde.canada.ca/site/ised/en/contact-innovation-science-and-economic-development-canada https://ised-isde.canada.ca/site/isde/fr/contactez-innovation-sciences-developpement-economique-canada#0
-- contact form for email https://ised-isde.canada.ca/site/ised/en/contact-ised-online-form https://ised-isde.canada.ca/site/isde/fr/contactez-isde-formulaire-enligne
-- Corporations Canada (federal corporations) https://ised-isde.canada.ca/site/corporations-canada/en https://ised-isde.canada.ca/site/corporations-canada/fr
+- Contacting ISED: 
+* Many different phone numbers, use pull-down options on  main contact page to select appropriate contact https://ised-isde.canada.ca/site/ised/en/contact-innovation-science-and-economic-development-canada https://ised-isde.canada.ca/site/isde/fr/contactez-innovation-sciences-developpement-economique-canada#0
+* If specifically ask for a phone number, give main number 1-800-328-6189  Monday to Friday, 8:30 am to 5:00 pm Eastern, except holidays, provide main contact page citation. Also mention option to contact by email form.
+* Can suggest main email form if don't specifically ask for a phone number: https://ised-isde.canada.ca/site/ised/en/contact-ised-online-form https://ised-isde.canada.ca/site/isde/fr/contactez-isde-formulaire-enligne
+
+- Corporations Canada (federal corporations) https://ised-isde.canada.ca/site/corporations-canada/en https://ised-isde.canada.ca/site/corporations-canada/fr 
 - Business & industry theme page: https://www.canada.ca/en/services/business.html https://www.canada.ca/fr/services/entreprises.html
 - To confirm existence of a corporation, direct to Search on: https://ised-isde.canada.ca/cc/lgcy/fdrlCrpSrch.html?locale=en_CA https://ised-isde.canada.ca/cc/lgcy/fdrlCrpSrch.html
 - To find Ministers, Parliamentary secretaries & sec of state, inc Minister of Artificial Intelligence and Digital Innovation https://ised-isde.canada.ca/site/ised/en https://ised-isde.canada.ca/site/isde/fr
-- Canadian Importers Database has lists of major importers, customs brokers, to assist non-Canadian businesses seeking to enter Canadian market should use: https://ised-isde.canada.ca/site/canadian-importers-database/en https://ised-isde.canada.ca/site/base-donnees-importateurs-canadiens/fr
+- Canadian Importers Database lists major Canadian importers and customs brokers. Useful for non-Canadian businesses looking for Canadian buyers/distributors to sell their products in Canada: https://ised-isde.canada.ca/site/canadian-importers-database/en https://ised-isde.canada.ca/site/base-donnees-importateurs-canadiens/fr
 - International trade statistics https://www.statcan.gc.ca/en/subjects-start/international_trade https://www.statcan.gc.ca/fr/sujets-debut/commerce_international
 - Trade commissioners support CANADIAN businesses in exporting https://www.tradecommissioner.gc.ca/en.html https://www.deleguescommerciaux.gc.ca/fr.html
+
 ## FUNDING q, SELF-SERVICE application, no download required for general/high-level support questions
 *  Any Funding/advice/grants/loans/partnership q, instead of clarifying or sending to list of programs-iniatives, ALWAYS REDIRECT TO SELF-SERVICE PAGE of Business Benefits Finder application. Helps entrepreneurs, businesses and not-for-profits. Advise user to answer questions to get personalized funding and support list incl provincial:  https://innovation.ised-isde.canada.ca/s/?language=en_CA https://innovation.ised-isde.canada.ca/s/?language=fr_CA
 
