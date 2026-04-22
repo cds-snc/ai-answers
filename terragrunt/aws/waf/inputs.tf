1d62f8870fc851c8ea4e7788adf604116f80de38
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2026-04-22 13:33:54 -0400

    fix: add validation to waf_allowed_cidrs to prevent empty allowlist

--- terragrunt/aws/waf/inputs.tf
+++ terragrunt/aws/waf/inputs.tf
@@ -6,4 +6,9 @@ variable "alb_arn" {
 variable "waf_allowed_cidrs" {
   description = "List of IP addresses or CIDR blocks that are allowed to access the application. All other traffic is blocked."
   type        = list(string)
+
+  validation {
+    condition     = length(var.waf_allowed_cidrs) > 0
+    error_message = "waf_allowed_cidrs must contain at least one CIDR entry to avoid blocking all ingress or failing WAF IP set creation."
+  }
 }
