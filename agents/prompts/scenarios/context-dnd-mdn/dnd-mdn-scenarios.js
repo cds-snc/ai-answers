016dcad7123a558ef8fe37d3c5091a13d22753bc
Lisa Fast <lisa@vation.ca>
2026-06-10 14:37:59 -0400

    fix: pull out military transitions out of dnd scenario
    and adjust agenticBase.js to not include rawgithubusercontent in download limits.

--- agents/prompts/scenarios/context-dnd-mdn/dnd-mdn-scenarios.js
+++ agents/prompts/scenarios/context-dnd-mdn/dnd-mdn-scenarios.js
@@ -10,17 +10,21 @@
 //   ONDCAF       Office of the Ombudsman for DND and the Canadian Armed Forces
 export const DND_MDN_SCENARIOS = `
 ## Transition to post-military life
-- Digital transition https://military-transition.canada.ca/en/ https://military-transition.canada.ca/fr
-- Overview https://www.canada.ca/en/department-national-defence/corporate/reports-publications/transition-guide/introduction.html https://www.canada.ca/fr/ministere-defense-nationale/organisation/rapports-publications/guide-de-transition/introduction.html
-- Medical support/return to duty https://www.canada.ca/en/ombudsman-national-defence-forces/education-information/caf-members/career/release-transition-civilian/caf-transition-group.html https://www.canada.ca/fr/ombudsman-defense-nationale-forces/information-educative/militaires-fac/carriere/liberation-transition-civil/groupe-transition-fac.html
-- Interim Reconstitution Employment Measure (IREM)https://www.canada.ca/en/department-national-defence/corporate/policies-standards/canadian-forces-military-personnel-instructions/interim-reconstitution-employment-measure.html https://www.canada.ca/fr/ministere-defense-nationale/organisation/politiques-normes/instructions-du-personnel-militaire-des-forces-canadiennes/mesure-demploi-provisoire-de-la-reconstitution.html
+### Single authoritative source for questions related to military transitions: ⚠️DOWNLOAD https://raw.githubusercontent.com/cds-snc/ai-answers/main/agents/prompts/scenarios/context-dnd-mdn/dnd-mdn-transition-scenarios.md
+* When to download: ANY question related to military transitions including:
+- Medical support/return to duty, casualty support for members/families in illness, injury or death
+- Interim Reconstitution Employment Measure (IREM) 
+- voluntary, compulsory (medical), and Reserve Force release from CAF & component transfers from/to  Primary & Supplementary Reserve
 
 ## Benefits, pay, services
 * topic https://www.canada.ca/en/government/publicservice/benefitsmilitary.html https://www.canada.ca/fr/gouvernement/fonctionpublique/avantagesmilitaires.html
 * pay https://www.canada.ca/en/department-national-defence/services/benefits-military/pay-pension-benefits/pay.html https://www.canada.ca/fr/ministere-defense-nationale/services/avantages-militaires/solde-pension-indemnites/solde.html
 * medical and dental CAF members https://www.canada.ca/en/department-national-defence/services/benefits-military/pay-pension-benefits/benefits/medical-dental.html https://www.canada.ca/fr/ministere-defense-nationale/services/avantages-militaires/solde-pension-indemnites/prestations/medicales-dentaires.html
 
+* Careers/recruiting https://forces.ca/en/careers https://forces.ca/fr/carrieres
+
 ## Defence organizations
+* CAF/FAC https://www.canada.ca/en/services/defence/caf.html https://www.canada.ca/fr/services/defense/fac.html
 * Defence construction Canada, crown corporation https://www.dcc-cdc.gc.ca/ https://www.cdc-dcc.gc.ca/
 * Defence investment agency - a special operating agency within Public Services and Procurement Canada https://www.canada.ca/en/defence-investment-agency.html https://www.canada.ca/fr/agence-investissement-defense.html
 * Bureau of Research, Engineering and Advanced Leadership in Innovation and Science (BOREALIS) https://www.canada.ca/en/department-national-defence/programs/borealis.html https://www.canada.ca/fr/ministere-defense-nationale/programmes/borealis.html
