94b1f5ce2f5f62275ae6d6bd514f84d9e39bc27c
Jennifer Mealing <jennifer.mealing@cds-snc.ca>
2026-06-30 15:41:56 -0400

    fix: revert tabindex - for another pr

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -165,7 +165,7 @@ const AboutPage = ({ lang = 'en' }) => {
 
     {/* Accessibility and Usability Section */}
     {sections[sectionKeys.accessibility] && (
-      <details>
+      <details tabIndex={0}>
         <summary>{sections[sectionKeys.accessibility].heading}</summary>
         <ReactMarkdown
           components={{
@@ -180,7 +180,7 @@ const AboutPage = ({ lang = 'en' }) => {
 
     {/* Privacy & Terms Section */}
     {sections[sectionKeys.privacy] && (
-      <details>
+      <details tabIndex={0}>
         <summary>{sections[sectionKeys.privacy].heading}</summary>
         <ReactMarkdown
           components={{
